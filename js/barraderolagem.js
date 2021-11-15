237a4076bcc43f5facddd49f6db7ffd64c45a3f8
HannaSantos <hanna.santos@bandtec.com.br>
2021-11-15 10:36:58 -0300

    atualizando

---
+++ js/barraderolagem.js
@@ -0,0 +1,7 @@
+let progress = document.getElementById("progressbar");
+  let totalheight = document.body.scrollHeight - window.innerHeight;
+  window.innerHeight;
+  window.onscroll = function () {
+    let progressHeigth = (window.pageYOffset / totalheight) * 100;
+    progress.style.height = progressHeigth + "%";
+  };
