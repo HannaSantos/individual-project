db051338e810ae41c3613f4ef78667b3b7e50f40
HannaSantos <hanna.santos@bandtec.com.br>
2021-12-02 16:26:24 -0200

    Ajuste

--- site/api-site/src/database/sql.sql
+++ site/api-site/src/database/sql.sql
@@ -5,31 +5,44 @@ create DATABASE arte;
 
 USE arte;
 
-create table votacao(
-idvota int primary key auto_increment,
-arte varchar(50)
-);
-
 CREATE TABLE usuario (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
 	senha VARCHAR(50),
-    telefone varchar(13),
-    fkvota int,
-    foreign key (fkvota) references votacao(idvota)
+    telefone int
 );
 
-insert into votacao
+create table tbArte(
+idarte int primary key auto_increment,
+arte varchar(50)
+);
+
+create table usuarioVoto (
+idvoto int primary key auto_increment,
+fkusuario int,
+fkarte int,
+foreign key (fkusuario) references usuario(id),
+foreign key (fkarte) references tbArte(idarte)
+);
+
+insert into tbArte
 value (1, "Escultura"),
       (2, "Nouveau"),
       (3, "barroco");
       
-update usuario set fkvota = 1 where id = 1;
+select * from usuarioVoto;
 
-select fkvota from usuario;
+select ar.arte, count(fkarte) as "Total de artes" from usuarioVoto us
+join tbArte ar on us.fkarte = ar.idarte
+where idarte = 1;
 
-select count(fkvota) as "Total" from usuario;
+select ar.arte, count(fkarte) as "Total de artes" from usuarioVoto us
+join tbArte ar on us.fkarte = ar.idarte
+where idarte = 2;
 
-select * from usuario;
-select * from votacao;
+select ar.arte, count(fkarte) as "Total de artes" from usuarioVoto us
+join tbArte ar on us.fkarte = ar.idarte
+where idarte = 3;
+
+select fkarte from usuarioVoto;
